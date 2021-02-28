595181b0bfebb02f1516cd5a28891c5ecb2ded9e
ExLZyor <brice.vachez@insa-lyon.fr>
2021-02-28 16:33:26 +0100

    Added doc to Cell and GraphArc

--- Sudoku/Sudoku Graphic/Cell.cs
+++ Sudoku/Sudoku Graphic/Cell.cs
@@ -6,31 +6,66 @@ using System.Threading.Tasks;
 
 namespace Sudoku_Graphic
 {
+
     public class Cell
     {
         #region Attributes
 
+        /// <summary>
+        /// The x position of the cell in the sudoku grid.
+        /// </summary>
         private int posX;
+        /// <summary>
+        /// Gets and sets the x position of the cell.
+        /// </summary>
         public int PosX { get => posX; set => posX = value; }
 
+        /// <summary>
+        /// The y position of the cell in the sudoku grid.
+        /// </summary>
         private int posY;
+        /// <summary>
+        /// Gets and sets the y position of the cell.
+        /// </summary>
         public int PosY { get => posY; set => posY = value; }
 
+        /// <summary>
+        /// The character inside the cell. If the cell is empty, it is represented by a '.'
+        /// </summary>
         private char value;
+        /// <summary>
+        /// Gets and sets the value of the cell.
+        /// </summary>
         public char Value { get => value; set => this.value = value; }
 
+        /// <summary>
+        /// The possible values (different than '.') that the field value can take. 
+        /// </summary>
         private List<char> domain;
+        /// <summary>
+        /// Gets and sets the domain of the cell.
+        /// </summary>
         public List<char> Domain { get => domain; set => domain = value; }
         #endregion
 
         #region Ctors
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Cell"/> class.
+        /// </summary>
+        /// <param name="x">The x position of the cell in the grid.</param>
+        /// <param name="y">The y position of the cell in the grid.</param>
         public Cell(int x = 0, int y = 0)
         {
             posX = x;
             posY = y;
             domain = new List<char>(new char[] { '1', '2', '3', '4', '5', '6', '7', '8', '9' });
         }
-
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Cell"/> class by copying
+        /// another <see cref="Cell"/> instance.
+        /// </summary>
+        /// <param name="cell">The cell to copy.</param>
         public Cell(Cell cell)
         {
             posX = cell.PosX;
@@ -41,7 +76,13 @@ namespace Sudoku_Graphic
         #endregion
 
         #region Operators
-
+        /// <summary>
+        /// Tests the equality between the fields of two <see cref="Cell"/> instances.
+        /// </summary>
+        /// <param name="cell2">The <see cref="Cell"/> on which the test is realized.</param>
+        /// <returns>
+        ///   <c>true</c> if the specified <see cref="Cell"/>'s fields are equal to this instance; otherwise, <c>false</c>
+        /// </returns>
         protected bool Equals(Cell cell2)
         {
             return posX == cell2.PosX &&
@@ -50,6 +91,13 @@ namespace Sudoku_Graphic
                 domain.All(cell2.Domain.Contains);
         }
 
+        /// <summary>
+        /// Determines whether the specified <see cref="System.Object" />, is equal to this instance.
+        /// </summary>
+        /// <param name="obj">The <see cref="System.Object" /> to compare with this instance.</param>
+        /// <returns>
+        ///   <c>true</c> if the specified <see cref="System.Object" /> is equal to this instance; otherwise, <c>false</c>.
+        /// </returns>
         public override bool Equals(object obj)
         {
             if (ReferenceEquals(null, obj)) return false;
@@ -58,6 +106,12 @@ namespace Sudoku_Graphic
             return Equals((Cell)obj);
         }
 
+        /// <summary>
+        /// Returns the hashcode of this instance of <see cref="Cell"/>.
+        /// </summary>
+        /// <returns>
+        /// The hashcode of this instance of <see cref="Cell"/>.
+        /// </returns>
         public override int GetHashCode()
         {
             int hash = posX.GetHashCode()^5 +
--- Sudoku/Sudoku Graphic/GraphArc.cs
+++ Sudoku/Sudoku Graphic/GraphArc.cs
@@ -9,12 +9,23 @@ namespace Sudoku_Graphic
     public class GraphArc
     {
         #region Attributes
+        /// <summary>
+        /// The first <see cref="Cell"/> connected by this instance of <see cref="GraphArc"/> in a graph.
+        /// </summary>
         private Cell cell1;
 
+        /// <summary>
+        /// The second <see cref="Cell"/> connected by this instance of <see cref="GraphArc"/> in a graph.
+        /// </summary>
         private Cell cell2;
         #endregion
 
         #region Ctors
+        /// <summary>
+        /// Initializes a new instance of the <see cref="GraphArc"/> class.
+        /// </summary>
+        /// <param name="cell1">The first instance of <see cref="Cell"/> connected by this <see cref="GraphArc"/></param>
+        /// <param name="cell2">The second instance of <see cref="Cell"/> connected by this <see cref="GraphArc"/></param>
         public GraphArc(Cell cell1, Cell cell2)
         {
             this.cell1 = cell1;
@@ -23,6 +34,12 @@ namespace Sudoku_Graphic
         #endregion
 
         #region Public Methods
+        /// <summary>
+        /// Checks whether the constraint represented by this instance of <see cref="GraphArc"/> is respected.
+        /// </summary>
+        /// <returns>
+        ///   <c>true</c> if the constraint is respected; otherwise, <c>false</c>
+        /// </returns>
         public bool IsConsistant()
         {
             if(cell1.Value == '.' || cell2.Value == '.')
@@ -32,18 +49,43 @@ namespace Sudoku_Graphic
             return cell1.Value != cell2.Value;
         }
 
+        /// <summary>
+        /// Checks whether or not this <see cref="GraphArc"/> and another 
+        /// instance of <see cref="GraphArc"/> connect the same two instances of <see cref="Cell"/>. 
+        /// </summary>
+        /// <param name="arc2">The second instance of <see cref="GraphArc"/> to test.</param>
+        /// <returns>
+        ///   <c>true</c> if both instances connect the same cells; otherwise, <c>false</c>
+        /// </returns>
         public bool IsDuplicata(GraphArc arc2)
         {
             return this.cell1 == arc2.cell1 &&
                 this.cell2 == arc2.cell2;
         }
 
-
+        /// <summary>
+        /// Checks if the given instance of <see cref="Cell"/> is one of the two <see cref="Cell"/>
+        /// connected by this arc.
+        /// </summary>
+        /// <param name="cell">The instance of the <see cref="Cell"/></param>
+        /// <returns>
+        ///   <c>true</c> if the <see cref="Cell"/> is one of the two attributes of this 
+        ///   instance of <see cref="GraphArc"/>; otherwise, <c>false</c>
+        /// </returns>
         public bool IsCellIn(Cell cell)
         {
             return cell1 == cell || cell2 == cell;
         }
-
+        /// <summary>
+        /// Given one instance of a <see cref="Cell"/>, returns the value of the other <see cref="Cell"/>
+        /// connected by this arc.
+        /// </summary>
+        /// <param name="cell">The first instance of <see cref="Cell"/> connected to this arc.</param>
+        /// <returns>
+        /// The field value of cell1 if <param name="cell"> is equal to cell2.
+        /// The field value of cell2 if <param name="cell"> is equal to cell1.
+        /// ' ' otherwise.
+        /// </returns>
         public char GetOtherCellValue(Cell cell)
         {
             if (cell == cell1)
@@ -59,12 +101,27 @@ namespace Sudoku_Graphic
         #endregion
 
         #region Operators
+
+        /// <summary>
+        /// Tests the equality between the fields of two <see cref="GraphArc"/> instances.
+        /// </summary>
+        /// <param name="arc2">The <see cref="GraphArc"/> on which the test is realized.</param>
+        /// <returns>
+        ///   <c>true</c> if the specified <see cref="GraphArc"/>'s fields are equal to this instance; otherwise, <c>false</c>
+        /// </returns>
         protected bool Equals(GraphArc arc2)
         {
             return cell1.Equals(arc2.cell1) &&
                 cell2.Equals(arc2.cell2);
         }
 
+        /// <summary>
+        /// Determines whether the specified <see cref="System.Object" />, is equal to this instance.
+        /// </summary>
+        /// <param name="obj">The <see cref="System.Object" /> to compare with this instance.</param>
+        /// <returns>
+        ///   <c>true</c> if the specified <see cref="System.Object" /> is equal to this instance; otherwise, <c>false</c>.
+        /// </returns>
         public override bool Equals(object obj)
         {
             if (ReferenceEquals(null, obj)) return false;
@@ -73,6 +130,12 @@ namespace Sudoku_Graphic
             return Equals((GraphArc)obj);
         }
 
+        /// <summary>
+        /// Returns the hashcode of this instance of <see cref="GraphArc"/>.
+        /// </summary>
+        /// <returns>
+        /// The hashcode of this instance of <see cref="GraphArc"/>.
+        /// </returns>
         public override int GetHashCode()
         {
             return cell1.GetHashCode() + 15 * cell2.GetHashCode() ^ 2;
